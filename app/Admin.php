8e4301fb465182b8387750b8c7501ebe755990f9
z9gmoon <z9gcaptcha@gmail.com>
2018-07-25 00:03:54 +0700

    make auth

--- app/Admin.php
+++ app/Admin.php
@@ -2,9 +2,14 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Admin extends Model
+class Admin extends Authenticatable
 {
-    //
+    use Notifiable;
+
+
+    protected $fillable = ['name','email','password', 'role'];
+    protected $hidden = ['password', 'remember_token'];
 }
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,6 @@ Route::group(['prefix' => 'admin', 'namespace' => 'Admin','as'=> 'admin.'], func
     Route::resource('admin','AdminController');
     
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
